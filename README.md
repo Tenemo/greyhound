a5bd25c422509a3aa0d2d74ca8d406ea28571f4c
Piotr Piech <piotr@piech.dev>
2021-03-26 09:58:49 +0100

    partition information

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## Greyhound
 
-Monitor /home/\$USER partition free space. Automatically prompt user with a list of suggested files to delete. Files to delete are suggested based on their size.
+Monitors free space on the partition containing /home/\$USER directory. Automatically prompt user with a list of suggested files to delete. Files to delete are suggested based on their size.
+
+Todo:
+ - support for -R --refresh [NUM] time argument
+ - support for -I --ignore argument that doesn't prompt for file deletion
--- hdguard.sh
+++ hdguard.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+showTitle() {
+    echo ""
+    echo "    _    _ _____       _____                     _ "
+    echo "   | |  | |  __ \     / ____|                   | |"
+    echo "   | |__| | |  | |   | |  __ _   _  __ _ _ __ __| |"
+    echo "   |  __  | |  | |   | | |_ | | | |/ _\` | '__/ _\` |"
+    echo "   | |  | | |__| |   | |__| | |_| | (_| | | | (_| |"
+    echo "   |_|  |_|_____/     \_____|\__,_|\__,_|_|  \__,_|"
+    echo ""
+}
+
+showPartitionInformation() {
+    partition=$(df /home/$USER --output=source | tail -1)
+    echo "  Partition containing /home/\$USER:  $partition"
+
+    disk="$(lsblk -no pkname $partition)"
+    echo "  Disk containing $partition:         $disk"
+
+    totalSpace="$(df /home/$USER -h --output=size | tail -1)"
+    echo "  Partition size:                   $totalSpace"
+
+    usedSpace="$(df /home/$USER -h --output=used | tail -1)"
+    echo "  Used space:                       $usedSpace"
+
+    availableSpace="$(df /home/$USER -h --output=avail | tail -1)"
+    echo "  Available space:                  $availableSpace"
+
+    echo ""
+}
+
 help() {
     echo "hdguard [LIMIT] [-h]"
     echo ""
@@ -9,7 +39,7 @@ help() {
     echo ""
     echo "  LIMIT          percentage of taken up partition space at which"
     echo "                   the script starts warning the user;"
-    echo "                   numeric value, from 10 to 95"
+    echo "                   numeric value, from 1 to 99"
     echo ""
     echo "Options:"
     echo "  -h, --help     display the help message"
@@ -40,27 +70,39 @@ getLimitFromArguments() {
         help
         exit 1
     fi
-    local limit="$argument"
+    limit="$argument"
     local numberRegex='^[0-9]+$'
     if ! [[ "$limit" =~ $numberRegex ]]; then
         echo "Passed an invalid size limit value."
         showRunWithHelp
         exit 1
     fi
-    if [[ "$limit" -lt 10 || "$limit" -gt 90 ]]; then
+    if [[ "$limit" -lt 1 || "$limit" -gt 99 ]]; then
         echo "Passed an invalid size limit value."
         showRunWithHelp
         exit 1
     fi
-    echo $limit
-    exit 0
 }
 
-limit=$(getLimitFromArguments $# $1)
+
+getLimitFromArguments $# $1
 
 while true; do
-    echo "$limit"
-    break
+    while true; do
+        clear
+        showTitle
+        showPartitionInformation
+        usagePercentage="$(df /home/$USER --output=pcent | tail -1 | tr -d '%')"
+        echo "  Taken up space user-set limit:     $limit%"
+        echo "  Current partition space usage:    $usagePercentage%"
+        echo ""
+        if [ $usagePercentage -gt $limit ]; then 
+            echo "LIMIT REACHED"
+            sleep 2
+            break
+        fi
+        sleep 60
+    done
 done
 
 exit 0
